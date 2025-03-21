067ec3f651a9c64bf199da86ed5f81bae5d0cbd8
= <minhducdtr70@gmail.com>
2025-03-21 21:03:13 +0700

    Update

--- product-service/product-service/src/main/java/com/example/product_service/controller/AdminProductController.java
+++ product-service/product-service/src/main/java/com/example/product_service/controller/AdminProductController.java
@@ -1,6 +1,68 @@
 
 package com.example.product_service.controller;
 
-public class AdminProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.product_service.entity.Product;
+import com.example.product_service.http.HeaderGenerator;
+import com.example.product_service.service.ProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private HeaderGenerator headerGenerator;
+
+    @PostMapping(value = "/products")
+    private ResponseEntity<Product> addProduct(@RequestBody Product product, HttpServletRequest request) {
+        if (product != null) {
+            try {
+                productService.addProduct(product);
+                return new ResponseEntity<Product>(
+                        product,
+                        headerGenerator.getHeadersForSuccessPostMethod(request, product.getId()),
+                        HttpStatus.CREATED);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Product>(
+                        headerGenerator.getHeadersForError(),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<Product>(
+                headerGenerator.getHeadersForError(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping(value = "/products/{id}")
+    private ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        Product product = this.productService.getProductById(id);
+        if (product != null) {
+            try {
+                this.productService.deleteProduct(id);
+                return new ResponseEntity<Void>(
+                        headerGenerator.getHeadersForSuccessGetMethod(),
+                        HttpStatus.OK);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Void>(
+                        headerGenerator.getHeadersForError(),
+                        HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<Void>(headerGenerator.getHeadersForError(), HttpStatus.NOT_FOUND);
+    }
 }
---
+++ product-service/product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -0,0 +1,81 @@
+package com.example.product_service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.product_service.entity.Product;
+import com.example.product_service.http.HeaderGenerator;
+import com.example.product_service.service.ProductService;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private HeaderGenerator headerGenerator;
+
+    @GetMapping(value = "/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProduct();
+        if (!products.isEmpty()) {
+            return new ResponseEntity<List<Product>>(
+                    products,
+                    headerGenerator.getHeadersForSuccessGetMethod(),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Product>>(
+                headerGenerator.getHeadersForError(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/products", params = "category")
+    public ResponseEntity<List<Product>> getAllProductByCategory(@RequestParam("category") String category) {
+        List<Product> products = productService.getAllProductByCategory(category);
+        if (!products.isEmpty()) {
+            return new ResponseEntity<List<Product>>(
+                    products,
+                    headerGenerator.getHeadersForSuccessGetMethod(),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Product>>(
+                headerGenerator.getHeadersForError(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/products/{id}")
+    public ResponseEntity<Product> getOneProductById(@PathVariable("id") long id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<Product>(
+                    product,
+                    headerGenerator.getHeadersForSuccessGetMethod(),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<Product>(
+                headerGenerator.getHeadersForError(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/products", params = "name")
+    public ResponseEntity<List<Product>> getAllProductsByName(@RequestParam("name") String name) {
+        List<Product> products = productService.getAllProductsByName(name);
+        if (!products.isEmpty()) {
+            return new ResponseEntity<List<Product>>(
+                    products,
+                    headerGenerator.getHeadersForSuccessGetMethod(),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Product>>(
+                headerGenerator.getHeadersForError(),
+                HttpStatus.NOT_FOUND);
+    }
+
+}
